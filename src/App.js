cc6387da11b4298e52c2577ad38660b21a7f2f08
Artem Vasylenko <irbiskronos@gmail.com>
2022-04-16 22:59:15 +0300

    3 commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import './styles/App.css';
 import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -13,7 +15,12 @@ function App() {
 
   return (
     <div className="App">
-        <PostItem post={} />
+        <form>
+            <input type="text" placeholder="Назва поста"/>
+            <input type="text" placeholder="Опис поста"/>
+            <MyButton>Створити пост</MyButton>
+        </form>
+        <PostList posts={posts} title="Список постів про JS"/>
     </div>
   );
 }
