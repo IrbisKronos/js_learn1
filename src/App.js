6a9b6084aa581a898589fe06f01a5501f73a76be
Artem Vasylenko <irbiskronos@gmail.com>
2022-04-22 21:05:26 +0300

    6

--- src/App.js
+++ src/App.js
@@ -11,10 +11,22 @@ import MySelectX from "./components/UI/select/MySelectX";
 
 function App() {
     const [posts, setPosts] = useState([
-        {id: 1, title: 'Javascript 1', body: 'Description'},
-        {id: 2, title: 'Javascript 2', body: 'Description'},
-        {id: 3, title: 'Javascript 3', body: 'Description'},
+        {id: 1, title: 'Javascript 1', body: 'ФDescription'},
+        {id: 2, title: 'МJavascript 2', body: 'УDescription'},
+        {id: 3, title: 'СJavascript 3', body: 'МDescription'},
     ])
+    const [selectedSort, setSelectedSort] = useState('')
+    const [searchQuery, setSearchQuery] = useState('')
+    
+    function getSortedPost() {
+        console.log('Спрацювала ф-я getSortedPost')
+        if(selectedSort){
+            return [...posts].sort((a,b) => a[selectedSort].localeCompare(b[selectedSort]))
+        }
+        return posts;
+    }
+
+    const sortedPosts = getSortedPost()
 
     // ф-я на вхід очикуватиме новий створений пост (newPost). Його ми будемо передавати в компоненті PostForm (setPost)
     // і все що ми робимо - змінюємо стан: розгортаємо старий масив і в кінці додаюмо новий пост
@@ -27,6 +39,10 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id))
     }
 
+    const sortPosts = (sort) => {
+        setSelectedSort(sort);
+    }
+
     // - <PostForm create={createPost}/> передаємо в наш компонент ф-ю зворотнього виклика. Сам props назвимо create,
     // а ф-ю - createPost
     // - removePost передаємо без дужок, як посилання, бо якщо передати з дужками, то ф-я просто викличеться
@@ -36,21 +52,30 @@ function App() {
         <PostForm create={createPost}/>
         <hr style={{margin: '15px 0'}}/>
         <div>
+            <MyInput
+                value={searchQuery}
+                onChange={e => setSearchQuery(e.target.value)}
+                placeholder="Пошук..."
+            />
             <MySelectX
+                value={selectedSort}
+                onChange={sortPosts}
                 defaultValue="Сортування"
-                option={[]}
+                options={[
+                    {value: 'title', name: 'По назві'},
+                    {value: 'body', name: 'По опису'},
+                ]}
             />
         </div>
         {posts.length !== 0
-        ? <PostList remove={removePost} posts={posts} title="Список постів про JS"/>
+        ? <PostList remove={removePost} posts={sortedPosts} title="Список постів про JS"/>
         :
             <h2 style={{textAlign: 'center'}}>
                 Постів не знайдено
             </h2>
         }
-
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/UI/select/MySelectX.jsx
+++ src/components/UI/select/MySelectX.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const MySelectX = ({options, defaultValue}) => {
+const MySelectX = ({options, defaultValue, value, onChange}) => {
     return (
-        <select>
-            <option value="">{defaultValue}</option>
+        <select
+            value={value}
+            onChange={event => onChange(event.target.value)}
+        >
+            <option disabled={true} value="">{defaultValue}</option>
             {options.map(option =>
-                <option value={option.value}>
+                <option key={option.value} value={option.value}>
                     {option.name}
                 </option>
             )}
